dde79bcc022c1f34c92c7896a33c1a07015cc133
MOJAAD <mojaad@outlook.com>
2020-07-27 22:19:27 +0430

    Hbox added

---
+++ course14(advance).py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import *
+
+class window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(400,100,450,400)
+        self.setWindowTitle("RSP-GAME!")
+        self.UI()
+
+    def UI(self):
+        hbox=QHBoxLayout()
+        btn1=QPushButton("button1",self)
+        btn2=QPushButton("button2")
+        btn3=QPushButton("button3")
+        hbox.addStretch()
+        hbox.addWidget(btn1)
+        hbox.addWidget(btn2)
+        hbox.addWidget(btn3)
+        hbox.addStretch()
+        self.setLayout(hbox)
+        self.show()
+
+
+
+def main():
+    App = QApplication(sys.argv)
+    Window = window()
+    sys.exit(App.exec_())
+
+if __name__ == '__main__':
+    main()
